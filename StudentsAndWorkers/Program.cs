a066168a8464dc8142397246d9aa1d8d17b53b0b
Birnat Ieronim <Ieronimb@outlook.com>
2019-05-13 21:43:46 +0300

    modified namespace

--- StudentsAndWorkers/Program.cs
+++ StudentsAndWorkers/Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace StudentsAndWorkers
+namespace Week3_Tema2_Ecercitiul3_StudentsAndWorkers
 {
     class Program
     {
